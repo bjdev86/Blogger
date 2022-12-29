92d4a4ff9febfa082692d25e78146ed01863e490
Ben Miller <bjmiller86@gmail.com>
2022-12-29 00:09:06 -0800

    Trying to make a singleton instance of the blogchannel service avialable to all sub components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogChannelService } from './services/blogchannel.service';
+import { BlogpostComponent } from './blogpost/blogpost.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogpostComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [BlogChannelService],
+  bootstrap: [AppComponent, BlogChannelService]
 })
 export class AppModule { }
