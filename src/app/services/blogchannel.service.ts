32565a55348ae76f01c122693170bfce4eb41102
Ben Miller <bjmiller86@gmail.com>
2022-12-29 00:04:05 -0800

    Converting to HTTP service.

--- src/app/services/blogchannel.service.ts
+++ src/app/services/blogchannel.service.ts
@@ -1,13 +1,26 @@
-import { Injectable } from '@angular/core';
+/**
+ * Service to fetch and transmit blog posts along with their replies to and
+ * from the Blogger API.
+ */
 
-@Injectable({
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+//import { http}
+@Injectable(
+{
   providedIn: 'root'
 })
 export class BlogChannelService
 {
-
+  // Class constructor
   constructor()
   {
 
   }
+
+  /* Functoin to fetch all blog posts from the API */
+//   getAllBlogPosts(): Observable<JSON>
+//   {
+//     return;
+//   }
 }
