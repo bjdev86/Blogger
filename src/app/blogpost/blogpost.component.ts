fb0724cfcf12e121ee486e29d2fc8add378625a0
Ben Miller <bjmiller86@gmail.com>
2022-12-29 00:13:56 -0800

    Added a sub-component called 'blogpost'. This component will hold and display the original blog post from the API.

---
+++ src/app/blogpost/blogpost.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component(
+{
+  selector: 'blogpost',
+  templateUrl: 'blogpost.html',
+  styleUrls: ['./blogpost.component.scss']
+})
+export class BlogpostComponent implements OnInit
+{
+  // Life cycle functoins
+  ngOnInit(): void
+  {
+
+  }
+
+  // Class constructor
+  constructor() { }
+
+
+}
