b2ae446566cc8380d4d5b8118cbddae5093a5177
Ben Miller <bjmiller86@gmail.com>
2022-12-30 00:11:26 -0800

    Added 'replypost' component. This component will house reply posts with thier attributes.

---
+++ src/app/blogpost/replypost/replypost.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-replypost',
+  template: `
+    <p>
+      replypost works!
+    </p>
+  `,
+  styleUrls: ['./replypost.component.scss']
+})
+export class ReplypostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
