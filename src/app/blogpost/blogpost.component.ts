513b11cf9c56ae055437af41ceed1cde0c20a474
Ben Miller <bjmiller86@gmail.com>
2022-12-30 00:06:22 -0800

    Preparing the 'blogpost' component to implement the blog post, reply posts and main post reply.

--- src/app/blogpost/blogpost.component.ts
+++ src/app/blogpost/blogpost.component.ts
@@ -1,21 +1,50 @@
+/**
+ * @author Ben Miller
+ * @date 12/29/2022
+ *
+ * @description Component to manage and host the original blog post along with
+ * any reply chains and comment editor. This component is the parent component
+ * of this application and hots the blog reply component and reply editor
+ * component as child components.
+ */
 import { Component, OnInit } from '@angular/core';
+import { BlogChannelService } from '../services/blogchannel.service';
 
 @Component(
 {
   selector: 'blogpost',
   templateUrl: 'blogpost.html',
-  styleUrls: ['./blogpost.component.scss']
+  styleUrls: ['./blogpost.component.scss'],
+  providers: [BlogChannelService]
 })
 export class BlogpostComponent implements OnInit
 {
+  // Class Member Declaration
+  public blogPost: string;
+  public topLevelReplies: Record<string, string>;
+
   // Life cycle functoins
   ngOnInit(): void
   {
+    // Retrieve the blogPost and its replies from the blogger API
+
+    // Parse blogPost from replies
 
   }
 
   // Class constructor
-  constructor() { }
+  constructor(blogChannel: BlogChannelService)
+  {
+    // Intialize public data members
+    this.blogPost = "";
+    this.topLevelReplies = {};
 
+    
+  }
 
+  // Function to issue a post to the API
+  public submitPost(post: string): void
+  {
+
+  }
 }
